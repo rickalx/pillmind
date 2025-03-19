f71e04c1f25f6e7f378fe182a7ab5db2107b2a5f
rick <rcrdfj@gmail.com>
2025-03-19 12:42:36 -0500

    deploy

--- pillmind/settings.py
+++ pillmind/settings.py
@@ -101,7 +101,7 @@ DATABASES = {
     ),
 }
 
-POSTGRES_LOCALLY = False
+POSTGRES_LOCALLY = True
 if ENVIRONMENT == 'production' or POSTGRES_LOCALLY == True:
     DATABASES['default']=dj_database_url.parse(os.getenv('DATABASE_PUBLIC_URL'))
 
