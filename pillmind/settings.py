f4dd87f3f7ef713e0bc2d2d0440488c119cc10ae
rick <rcrdfj@gmail.com>
2025-03-19 12:00:33 -0500

    deploy

--- pillmind/settings.py
+++ pillmind/settings.py
@@ -26,15 +26,23 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-w)gw-q)^glzor!1ee&jv!0wj(%jl#&4r-#6ax+ip#=&a^@kt6i'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+if ENVIRONMENT == 'development':
+    DEBUG = True
+else:
+    DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'pillmind.up.railway.app']
 
 CSRF_TRUSTED_ORIGINS = ['https://pillmind.up.railway.app']
 
+SECURE_SSL_REDIRECT = True
+
+SECURE_HSTS_SECONDS = 31536000  # 1 año en segundos
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True  # Aplica HSTS a subdominios
+SECURE_HSTS_PRELOAD = True  # Permite que tu dominio sea incluido en la lista de precarga de HSTS
 
 # Application definition
 
@@ -46,7 +54,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'webapp',
-    'corsheaders',#eliminar para producción
+    #'corsheaders',#eliminar para producción
 ]
 
 MIDDLEWARE = [
@@ -186,3 +194,6 @@ CORS_ALLOWED_ORIGINS = [
 LOGIN_REDIRECT_URL = '/'  # Redirige al inicio después de iniciar sesión
 LOGOUT_REDIRECT_URL = '/'  # Redirige al inicio después de cerrar sesión
 LOGIN_URL = '/accounts/login/'  # Ruta para la vista de inicio de sesión
+
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
